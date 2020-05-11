4eef1821e596362113919873224ab1521c7fd4a0
omar <omarasmar9955@gmail.com>
2020-05-11 11:27:13 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   "use strict";
 
   AOS.init();
@@ -9,3 +8,16 @@ $(document).ready(function () {
     time: 1000,
   });
 });
+
+var video = document.getElementById("video1");
+var palayButton = document.querySelector(".play-button");
+function playpause() {
+  console.log("hi");
+  if (video.paused) {
+    video.play();
+    palayButton.style.display = "none";
+    video.setAttribute("controls", "true");
+  } else {
+    video.pause();
+  }
+}
